f81aecd8e343dfe0edf3ad29fe72cea46eb91201
gajen55 <gajab55@gmail.com>
2023-08-07 15:00:13 +0530

    Update form.py

--- form.py
+++ form.py
@@ -1,28 +1,34 @@
 import streamlit as st
 import pandas as pd
-# List of names
+
+# Sample list of names
 NameList = ['Rohan', 'Roshan', 'Ankit', 'Anusha']
 
-def filter_names(search_string):
-    return [name for name in NameList if search_string.lower() in name.lower()]
+# Function to filter names based on input string
+def filter_names(input_string):
+    return [name for name in NameList if input_string.lower() in name.lower()]
 
+# Function to save responses to Excel
+def save_to_excel(name):
+    data = {'Selected Name': [name]}
+    df = pd.DataFrame(data)
+    with pd.ExcelWriter('responses.xlsx', mode='a') as writer:
+        df.to_excel(writer, index=False, header=not writer.sheets)
+
+# Streamlit app
 def main():
     st.title('Name Search Web App')
+
+    input_string = st.text_input('Enter a string:')
+    find_button = st.button('Find')
+
+    if find_button:
+        filtered_names = filter_names(input_string)
+        selected_name = st.selectbox('Select a name:', filtered_names)
+
+        if selected_name:
+            save_to_excel(selected_name)
+            st.success(f'You have selected: {selected_name}')
     
-    # Text input
-    search_input = st.text_input("Enter a string to search for names:")
-    
-    # Find button
-    if st.button('Find'):
-        filtered_names = filter_names(search_input)
-        
-        # Dropdown box
-        if len(filtered_names) > 0:
-            selected_name = st.selectbox('Select a name:', filtered_names)
-            st.write(f'You selected: {selected_name}')
-        else:
-            st.write('No matching names found.')
-            
-     
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
