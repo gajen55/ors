138cddd07a9d6d14ac7ee98a400101cf904ee2dd
gajen55 <gajab55@gmail.com>
2023-08-08 13:39:36 +0530

    Update form.py

--- form.py
+++ form.py
@@ -1,34 +1,50 @@
 import streamlit as st
-import pandas as pd
 
-# Sample list of names
+# List of names in the backend
 NameList = ['Rohan', 'Roshan', 'Ankit', 'Anusha']
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
 
-# Function to filter names based on input string
-def filter_names(input_string):
-    return [name for name in NameList if input_string.lower() in name.lower()]
-
-# Function to save responses to Excel
-def save_to_excel(name):
-    data = {'Selected Name': [name]}
-    df = pd.DataFrame(data)
-    with pd.ExcelWriter('responses.xlsx', mode='a') as writer:
-        df.to_excel(writer, index=False, header=not writer.sheets)
-
-# Streamlit app
-def main():
-    st.title('Name Search Web App')
-
-    input_string = st.text_input('Enter a string:')
-    find_button = st.button('Find')
-
-    if find_button:
-        filtered_names = filter_names(input_string)
-        selected_name = st.selectbox('Select a name:', filtered_names)
-
-        if selected_name:
-            save_to_excel(selected_name)
-            st.success(f'You have selected: {selected_name}')
+if 'selected_names' not in st.session_state:
+    st.session_state.selected_names = []
     
-if __name__ == '__main__':
+def set_clicked():
+    st.session_state.clicked = True
+    
+def main():
+    st.title('Name Finder Web App')
+    
+    # Text input for user to enter a string
+    user_input = st.text_input("Enter a string to find names:")
+    
+    # Button to trigger the name search
+    st.button("Find", on_click=set_clicked)
+    if user_input:
+        if st.session_state.clicked:
+            
+            found_names = [name for name in NameList if user_input.lower() in name.lower()]
+            if found_names:
+                st.write("Names containing the entered string:")
+                selected_names = st.selectbox("Select names:", found_names)
+                    
+                if selected_names:
+                    if(st.button("Add")):
+                        if selected_names not in st.session_state.selected_names:
+                            st.session_state.selected_names.append(selected_names)
+                    if(st.button("Remove")):
+                        if selected_names in st.session_state.selected_names:
+                            st.session_state.selected_names.remove(selected_names)
+
+                    st.write("Selected names:")
+                    
+                    st.write("\n\n\n\n\n")
+                    #st.write(st.session_state.selected_names)
+            else:
+                st.write("No names found with the entered string.")
+    else:
+        st.write("Please enter a string to find names.")
+    final_names = list(set(st.session_state.selected_names))
+    st.write(final_names)
+    
+if __name__ == "__main__":
     main()
